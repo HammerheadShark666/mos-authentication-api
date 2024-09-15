683cdd8653ff692ad8bbf1cc117716397767d4ff
HammerheadShark666 <103519731+HammerheadShark666@users.noreply.github.com>
2024-09-15 13:14:41 +0100

    Remove commented out code

--- Microservice.Authentication.Api/Extensions/IServiceCollectionExtensions.cs
+++ Microservice.Authentication.Api/Extensions/IServiceCollectionExtensions.cs
@@ -67,7 +67,6 @@ public static class IServiceCollectionExtensions
             SqlAuthenticationProvider.SetProvider(
                     sqlAuthenticationMethod,
                     sqlAuthenticationProvider);
-            // new DevelopmentAzureSQLProvider());
             var sqlConnection = new SqlConnection(connectionString);
             options.UseSqlServer(sqlConnection);
         });
